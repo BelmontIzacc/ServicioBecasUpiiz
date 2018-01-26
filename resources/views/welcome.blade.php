7ee5c1ee3927f016e703bc38d79ff58507d41ffb
AleBelmont <rebelde.alejandra@hotmail.com>
2018-01-26 17:45:33 -0600

    Segunda Entrega

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,99 @@
 <!DOCTYPE html>
+    <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <title>Registro</title>
+            <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+            <link href="/Templates/css/font-awesome.css" rel="stylesheet" type="text/css" />
+            <link href="/Templates/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+            <link href="/Templates/css/animate.css" rel="stylesheet" type="text/css" />
+            <link href="/Templates/css/admin.css" rel="stylesheet" type="text/css" />
+        </head>
+
+<body class="light_theme  fixed_header left_nav_fixed">
+    <div class="wrapper">
+
+    <!--\\\\\\\ wrapper Start \\\\\\-->
+
+    <div class="registration">
+    <div class="panel-heading border login_heading">Registrate</div>    
+ 
+    <form role="form" class="form-horizontal">
+      <div class="form-group">
+        <div class="col-sm-10">
+          <input type="text" placeholder="Nombre(s)" id="inputEmail3" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group">
+      <div class="col-sm-10">
+          <input type="text" placeholder="Apellido Paterno" id="inputEmail3" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group">
+      <div class="col-sm-10">
+          <input type="text" placeholder="Apellido Materno" id="inputEmail3" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group">
+      <div class="col-sm-10">
+          <input type="text" placeholder="Boleta" id="inputEmail3" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group">
+      <div class="col-sm-10">
+          <input type="text" placeholder="Semestre" id="inputEmail3" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group">
+      <div class="col-sm-10">
+          <input type="text" placeholder="Programa Académico" id="inputEmail3" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group">
+      <div class="col-sm-10">
+          <input type="text" placeholder="Grupo" id="inputEmail3" class="form-control">
+        </div>
+      </div>
+
+      <div class="form-group">
+      <div class="col-sm-10">
+          <input type="text" placeholder="Promedio Actual" id="inputEmail3" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        
+        <div class="col-sm-10">
+          <input type="text" placeholder="Periodo escolar actual" id="inputPassword3" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class=" col-sm-10">
+          <div class="checkbox checkbox_margin">
+              <button class="btn btn-default pull-right" type="submit">Siguiente</button>
+              </div>
+        </div>
+      </div>
+      
+    </form>
+ </div>
+  
+</div>
+<!--\\\\\\\ wrapper end\\\\\\-->
+
+<script src="/Templates/js/jquery-2.1.0.js"></script>
+<script src="/Templates/js/bootstrap.min.js"></script>
+<script src="/Templates/js/common-script.js"></script>
+<script src="/Templates/js/jquery.slimscroll.min.js"></script>
+</body>
+</html>
+
+<!--<!DOCTYPE html>
 <html>
     <head>
         <title>Laravel</title>
@@ -41,4 +136,4 @@
             </div>
         </div>
     </body>
-</html>
+</html>-->
